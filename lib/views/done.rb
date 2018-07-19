c450f910a8f35aa7e5b6a9ff1aebfa245f0db6b0
Léona Chevrel <chevrel.leona@gmail.com>
2018-07-20 00:23:25 +0200

    modification des noms de méthode dans done.rb

--- lib/views/done.rb
+++ lib/views/done.rb
@@ -1,15 +1,15 @@
 require "index"
 
 class Done
-	def verification_scrapper # Indique à l'utilisateur que le scrapping est terminé
+	def Done.verification_scrapper # Indique à l'utilisateur que le scrapping est terminé
 			puts "Le scrapping est terminé !"
 			Index.menu
 	end
-	def verification_mailer # Indique à l'utilisateur que le mailing est terminé
+	def Done.verification_mailer # Indique à l'utilisateur que le mailing est terminé
 			puts "Le mailing est terminé !"
 			Index.menu
 	end
-	def verification_follower # Indique à l'utilisateur que le following est terminé
+	def Done.verification_follower # Indique à l'utilisateur que le following est terminé
 			puts "Le following est terminé !"
 			Index.menu
   end
